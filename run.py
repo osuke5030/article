e9dbe3c89a7b69fe4acd48b792a245852b042dc5
osuke <o-suke.0108@docomo.ne.jp>
2022-07-05 14:59:01 +0900

    a

--- run.py
+++ run.py
@@ -156,7 +156,7 @@ def job():
 
                 
 def main():
-    schedule.every(1).minutes.do(job)
+    schedule.every(10).minutes.do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
